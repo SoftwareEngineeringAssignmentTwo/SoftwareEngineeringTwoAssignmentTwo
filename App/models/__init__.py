3029d0febb1cbe4226c874d3280ba025abd4c4cb
ranasalim1 <ranasalim4002@gmail.com>
2025-10-23 13:58:20 -0400

    Splitting user.py, edited __init__.py & added accolade.py, activitylog.py

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -1,1 +1,26 @@
-from .user import User, Student, Staff, ActivityLog, Accolade, LeaderBoardEntry
+from .user import User
+from .activitylog import ActivityLog
+from .accolade import Accolade
+from .leaderboardentry import LeaderBoardEntry
+from .student import Student
+from .staff import Staff
+
+# Handle circular dependencies
+import App.models.student
+import App.models.staff
+import App.models.activitylog
+import App.models.accolade
+import App.models.leaderboardentry
+
+# Make types available at module level
+ActivityLog.Student = Student
+Staff.Student = Student
+Staff.ActivityLog = ActivityLog
+Staff.Accolade = Accolade
+Staff.LeaderBoardEntry = LeaderBoardEntry
+Student.ActivityLog = ActivityLog
+Student.LeaderBoardEntry = LeaderBoardEntry
+Student.Accolade = Accolade
+LeaderBoardEntry.Student = Student
+
+__all__ = ['User', 'Student', 'Staff', 'Accolade', 'ActivityLog', 'LeaderBoardEntry']
